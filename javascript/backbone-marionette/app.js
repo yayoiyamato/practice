e10c90660dc1508905a5d059d87b70783955f98b
yayoiyamato <y.yamato.1234@gmail.com>
2020-05-12 23:30:04 +0900

    雛形

--- javascript/backbone-marionette/app.js
+++ javascript/backbone-marionette/app.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('OK');
+// View
+var Contact = Backbone.Marionette.ItemView.extend({
+    el: '#contact-region',
+    template: '#contact-template',
+});
+
+
+// Model
+var Person = Backbone.Model.extend({
+    defaults: {
+        name: 'Alice',
+        age: 20
+    }
+});
+   
+var person = new Person();
+var contact = new Contact({
+    model: person
+});
+
+contact.render();
