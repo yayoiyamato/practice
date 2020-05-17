5c19cb0cb822565b0b9dbe2fe9aa003fb33ec8e0
yayoiyamato <y.yamato.1234@gmail.com>
2020-05-17 23:58:11 +0900

    読み込みエラーなおった

--- javascript/backbone-marionette-handlebars/test/app.js
+++ javascript/backbone-marionette-handlebars/test/app.js
@@ -1,18 +1,26 @@
 'use strict'
+
 define([
     'jquery',
     'underscore',
     'backbone',      
     'backbone.marionette',
-    'backbone/backbone.marionette.handlebars',          
-    'hbs!Template/category',       
+    'backbone.marionette.handlebars',          
 ],
-    function ($, _, Backbone, Marionette, MarionetteHandlebars, categoryTpl) {
-        return Backbone.Marionette.ItemView.extend({
+    function ($, _, Backbone, Marionette, MarionetteHandlebars) {
+        return Marionette.LayoutView.extend({
 
             template: {
                 type: 'handlebars',
-                template: categoryTpl
+                template: '<p>name: {{ name }}</p>'
+            },
+            el: '.main',
+            initialize: function () {
+                console.log('initialize')
+            },
+            onBeforeShow: function () {
+                console.log('onBeforeShow')
+                this.render();
             }
 
         });
