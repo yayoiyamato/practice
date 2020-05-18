369787ade261ca288603446591de8f778beaa960
yayoiyamato <y.yamato.1234@gmail.com>
2020-05-18 23:38:10 +0900

    関数でも使う練習

--- javascript/object.js
+++ javascript/object.js
@@ -5,13 +5,29 @@
  */
 
 const box = { width: 50, height: 25, color: 'pink' };
-const cloneBox = {...box}
+const cloneBox = { ...box }
 console.log(cloneBox) //{ width: 50, height: 25, color: 'pink' }
 
 const size = { width: 50, height: 25 }
-const color = { color: 'pink'}
+const color = { color: 'pink' }
 const pinkBox = { ...color, size: { ...size } }
 console.log(pinkBox) // { color: 'pink', size: { width: 50, height: 25 } }
 
 // TODO: const color = 'pink'にしたらこうなる↓の何でかしらべる
 // { '0': 'p', '1': 'i', '2': 'n', '3': 'k', size: { width: 50, height: 25 }}
+
+
+const numbers = [1, 2, 3]
+
+// 関数の引数にも使える
+function sum(a, b, c) {
+    return a + b + c
+}
+console.log(sum(...numbers)) // 6
+
+// 関数の引数のどこでも使える
+function multiply (a, b, c, d, e) {
+    return a * b * c * d * e
+}
+console.log(multiply(-1, ...numbers, 5)) // -30
+
