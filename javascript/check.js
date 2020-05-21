85f64de8f8f59e326756e1f87242abb70d7649e6
yayoiyamato <y.yamato.1234@gmail.com>
2020-05-21 23:19:22 +0900

    有限数判定

--- javascript/check.js
+++ javascript/check.js
@@ -25,11 +25,12 @@ const zero = 0
  *  - 0.1 : 少数
  *  - {} : 空のオブジェクト
  *  - [] : 空の配列
+ *  - ±Infinity : 無限大
  * 
  *  # false
  *  - '' : 空
  *  - 0
- *  - NaN
+ *  - NaN(Number.NaN)
  *  - undefined
  *  - null
  */
@@ -40,6 +41,8 @@ console.log('plusNumber は' + check(plusNumber))
 console.log('floatNumber は' + check(floatNumber))
 console.log('emptyObject は' + check(emptyObject))
 console.log('emptyArray は' + check(emptyArray))
+console.log('Infinity は' + check(Infinity))
+console.log('-Infinity は' + check(-Infinity))
 console.log('* * * * * * * * * * * *')
 console.log('empty は' + check(''))
 console.log('zero は' + check(zero))
@@ -92,4 +95,23 @@ console.log('Number.isNaN(floatNumber) は' + check(Number.isNaN(floatNumber))) 
 console.log("Number.isNaN('') は" + check(Number.isNaN('')))  // false
 console.log('Number.isNaN(null) は' + check(Number.isNaN(null)))  // false
 console.log('Number.isNaN(undefined) は' + check(Number.isNaN(undefined)))  // false
+console.log('* * * * * * * * * * * *')
+
+
+/* 
+ *  有限数（無限大でない）判定
+ *
+ *  isFiniteは 引数が数値型ではない場合、まず数値型へと型変換された上で比較される
+ *  型変換を意図しない場合は Number.isFiniteは()を使用する
+ *  ※ Infinityに対して行った計算はInfinityになるが、Infinityで割った値は0になる
+ */ 
+console.log('isFinite(zero) は' + check(isFinite(zero)))  // true
+console.log("isFinite('') は" + check(isFinite('')))  // true
+console.log('isFinite(null) は' + check(isFinite(null)))  // true
+console.log('isFinite(undefined) は' + check(isFinite(undefined)))  // false
+console.log('* * * * * * * * * * * *')
+console.log('Number.isFinite(zero) は' + check(Number.isFinite(zero)))  // true
+console.log("Number.isFinite('') は" + check(Number.isFinite('')))  // false
+console.log('Number.isFinite(null) は' + check(Number.isFinite(null)))  // false
+console.log('Number.isFinite(undefined) は' + check(Number.isFinite(undefined)))  // false
 console.log('* * * * * * * * * * * *')
