153ade4f7383c853579c9b959499a7d127855f8b
yayoiyamato <y.yamato.1234@gmail.com>
2020-08-18 22:01:25 +0900

    modelの練習（プロパティ操作）

--- javascript/backbone-marionette/README.md
+++ javascript/backbone-marionette/README.md
@@ -1,9 +1,19 @@
 # Backbone.js
 
 ## Model
- * constructor(initialize)
+ * `constructor(initialize)`
    - インスタンス生成時に実行される
    - 両方定義するとconstructorが実行される
    - DOMと紐付けたい場合はinitializeを使用する
- * static プロパティ、メソッド
-   extendの第二引数に渡したオブジェクトが static メンバーになる
+ * `static` プロパティ、メソッド
+   - extendの第二引数に渡したオブジェクトが static メンバーになる
+   - 第一引数があったら設定されない？
+ * `defaults`
+   - プロパティのデフォルト値を設定する
+   - constructorがあったらdefaultsがきかない？undefinedになる
+ * `model.has('propertyName')`
+   - プロパティを持ってるかを調べる
+   - メソッドの存在は確認できない。
+ * `model.unset('propertyName')`
+   - プロパティを削除する
+   - メソッドには使えない
--- javascript/backbone-marionette/script.js
+++ javascript/backbone-marionette/script.js
@@ -4,24 +4,56 @@
  * Model
  */
 
- var MyModel1 = Backbone.Model.extend({
-  // コンストラクタを定義する
-  constructor: function() {
-    console.log('constructor')
-  },
-  initialize: function() {
-    console.log('initialize')
-  }
-})
-var myModel1 = new MyModel1() // constructor
-
-// static メンバーを定義する
-var MyModel2 = Backbone.Model.extend({}, {
-  staticProperty: 'static property',
-
-  staticMethod: function() {
-      console.log('static method')
+var staticModel = Backbone.Model.extend({}, {
+  // static メンバーを定義する
+  property: 'static property',
+  method: function() {
+    console.log('static method')
   }
 });
-console.log(MyModel2.staticProperty)　// static property
-MyModel2.staticMethod() // static method
+console.log(staticModel.property) // static property
+staticModel.method() // static method
+
+/* ----------------------------------------- */
+
+var MyModel = Backbone.Model.extend({
+  // コンストラクタを定義する
+  // constructorあったらdefaultsきかない？
+  // constructor: function() {
+  //   console.log('constructor')
+  // },
+  initialize: function(arg) {
+    console.log('initialize')
+    console.log(arg) // { name: 'yai' }
+  },
+  // default値を設定する
+  defaults: {
+    value: 'default value'
+  },
+  method: function() {
+    console.log('method')
+  }
+})
+var myModel = new MyModel() // constructor
+console.log(myModel.get('value')); // default value
+myModel.set('value', 'updated value');
+console.log(myModel.get('value')) // updated value
+
+
+// コンストラクタの引数でプロパティを設定
+var constructorModel = new MyModel({ name: 'yai'})
+console.log(constructorModel.get('name'))
+
+// プロパティを持ってるかを調べる
+console.log(myModel.has('value')) // true
+console.log(myModel.has('name')) // false
+console.log(constructorModel.has('name')) // true
+console.log(constructorModel.has('method')) // false
+
+// プロパティを削除する
+constructorModel.unset('name')
+console.log(constructorModel.get('name')) // undefined
+
+
+/* ----------------------------------------- */
+
