8f841f173fde3560cb41495e6f27f8e90730fd02
yayoiyamato <y.yamato.1234@gmail.com>
2020-09-05 23:40:12 +0900

    async 練習

--- javascript/async.js
+++ javascript/async.js
@@ -7,6 +7,7 @@
 // async ------------------------------------------------------------
 // 非同期関数を定義する関数宣言のこと。
 // async function は呼び出されると Promise を返す。
+// async は await とセットで使う必要はない。
 // async function が値を return した場合、Promiseは戻り値を resolve する。
 // async function が例外や何らかの値を throw した場合はその値を reject する。
 
@@ -15,35 +16,22 @@
 // 結果が返されたら async function 内の処理を再開する。
 // await は async function 内でないと利用できない。
 
-async function asyncSample(arg) {
-
-  // 非同期処理を定義
-  const hidouki = () => {
-    return new Promise((resolve, reject)=> {
-      setTimeout(() => {
-        if (arg) {
-          resolve("done!")
-        } else {
-          reject("fail!")
-        }
-      }, 2000)
-    })
-  }
-
-  // メイン処理
-  await hidouki().then((result) => {
-    console.log(result)
-    return result
-  }).catch((result) => {
-    console.log(result)
-    throw new Error(result)
-  })
+/* 普通の関数 */
+function normal() {
+  return 841;
 }
 
-asyncSample('xxx') // done!
+let num = normal()
+console.log(num) // 123
 
-asyncSample() 
-// fail!
-// UnhandledPromiseRejectionWarning: Error: fail!
-// UnhandledPromiseRejectionWarning: Unhandled promise rejection. ...
-// DeprecationWarning: Unhandled promise rejections are deprecated. ...
+
+/* async関数 */
+async function asyncFunc() {
+  return 841;
+}
+console.log(asyncFunc()) // Promise { 841 }：returnした値でresolveされるPromiseが返る
+
+asyncFunc().then(result => {
+  num = result
+  console.log(num) // 841
+})
